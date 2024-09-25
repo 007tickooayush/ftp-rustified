23e2e74173cf09784124504592c567969110d8b5
007tickooayush <ayush.tickoo292000@gmail.com>
2024-09-25 21:04:11 +0530

    removing handler attribute+implementing server run method

--- src/server.rs
+++ src/server.rs
@@ -1,16 +1,36 @@
+use std::net::{IpAddr, SocketAddr};
 use std::path::PathBuf;
-use tokio::runtime::Handle;
+use dotenv::dotenv;
+use tokio::net::TcpListener;
+use crate::ftp_config::FtpConfig;
 
 pub struct Server {
-    handle: Handle,
+
     root_dir_server: PathBuf,
+    ftp_config: FtpConfig
 }
 
 impl Server {
-    pub fn new(handle: Handle, root_dir_server: PathBuf) -> Self {
+    pub fn new(root_dir_server: PathBuf, ftp_config: FtpConfig) -> Self {
         Server {
-            handle,
             root_dir_server,
+            ftp_config
         }
+    }
+
+    pub async fn run(&self) {
+        dotenv().ok();
+        let addr = &self.ftp_config.addr;
+        let port = &self.ftp_config.port;
+
+        let socket_addr = SocketAddr::new(IpAddr::V4(addr.parse().unwrap()), *port);
+
+        println!("Client expected at {}", socket_addr);
+
+        let listener = TcpListener::bind(&socket_addr).await.unwrap();
+        loop {
+
+        }
+
     }
 }
